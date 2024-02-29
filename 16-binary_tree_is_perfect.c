534bff4e23a159e8be3e8ebc5c30c88ba1eb1288
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-02-29 19:41:19 +0100

    fixed the binary tree print file

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -4,5 +4,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
 }
