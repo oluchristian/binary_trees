47167a905f7075d406f1139c8e2889892bea951b
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-03-01 04:00:53 +0100

    a function that finds the uncle of a node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -11,6 +11,8 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
+	if (grandparent == NULL)
+        return (NULL);
 	if (grandparent->left && grandparent->right)
 	{
 		if (node->parent == grandparent->left)
